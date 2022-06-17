b60deeda9e38bddf46cef9c83d1612f7aed342c5
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-06-17 17:20:59 -0600

    Navigate commit message confusion

--- sketch.js
+++ sketch.js
@@ -6,16 +6,34 @@ function drawGrid() {
     output = "";
     for(row = 0;row < square; row++){
         box[row] = [];
-        output += "<div class='row'>";
+        output += "<div class='default'>";
         for(col = 0;col < square; col++){
             box[row][col] = ''
             box[row].push([col]);
-            output += "<div class='col'></div>";
+            output += `<div id="box[${row}][${col}]" class='default'></div>`;
         }
         output += "</div>";
     }
     document.getElementById('griddle').innerHTML = output;
 }
 drawGrid();
+
+colorDict = {
+    0: 'default',
+    1: 'white'
+}
+
+//function hover(div, white){
+//    div.addEventListener('mouseenter', e => div.classList.add('white'));
+//    div.addEventListener('mouseleave', e => div.classList.add('default'));
+//}
+//hover(document.querySelector('div'), 'white');
 griddle.forEach((div) => {
-    div.addEventListener('click', (e) => {console.log(e.target.id)})});
+    div.addEventListener('click', (e) => {e.target.classList.toggle('white');
+    div.addEventListener('mouseenter', e => div.classList.add('white'));
+    div.addEventListener('mouseleave', e => div.classList.add('default'));
+    
+    //const temp = e.target.id;
+    })
+});
+
