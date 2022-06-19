9ca3e756e5f7909a09e2097ee60c759981973b92
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-06-18 18:25:17 -0600

    Grid model successful, though, the dimensions of the grid still need to inhibit squares from populating further down the page

--- sketch.js
+++ sketch.js
@@ -1,41 +1,47 @@
-const griddle = document.querySelectorAll(`div`);
-var box = [], color = 'dark';
-const square = 16;
-var row = square, col = row;
-let mouseDown = false;
+let grid = document.getElementById('grid');
+var slider = document.getElementById("myRange"); 
+var color = 'dark';
+var side = slider.value;
+let mouseDown = false; 
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
-function hueStroke(e) {
+drawGrid(side);
+slider.onmouseup = function() {
+    wipeGrid();
+    side = slider.value;
+    
+    drawGrid(side);
+}    
+function wipeGrid() {
+    grid.innerHTML = '';
+}
+function drawGrid(side) {
+    let dimens = document.getElementById('grid').clientWidth / side;
+    grid.style.gridTemplateColumns = `repeat(${side - 3}, ${dimens}px) 1fr 1fr 1fr`;
+    grid.style.gridTemplateRows = `repeat(${side - 3}, ${dimens}px) 1fr 1fr 1fr`;
+    for(let i = 1; i < (side*side); i++){
+        let square = document.createElement('div')
+        grid.appendChild(square)
+        square.classList.add('dark')
+        square.style.width = dimens + 'px';
+        square.style.height = dimens + 'px';
+        //square.classList.add('border-top-left');
+        }
+}
+let squares = document.querySelectorAll('div')
+squares.forEach((square) => { 
+    square.addEventListener('mouseover', (e) => {if (mouseDown == true) {
+        e.target.classList.add(`light`)
+    }})
+})
+function canvasColor() {
     //if (e.type === 'mouseover' && !mouseDown) return
     switch (color) {
-        case 'dark':
-            this.classList.add('light');
-        case 'light':
-            this.classList.add('dark');
+        case (color == 'dark'):
+            color = 'light';
+            return color;
+        case (color == 'light'):
+            color = 'dark';
+            return color;
     }
-}
-
-
-function drawGrid(square) {
-    output = "";
-    for(row = 0;row < square; row++){
-        box[row] = [];
-        output += "<div class='dark'>";
-        for(col = 0;col < square; col++){
-            box[row][col] = ''
-            box[row].push([col]);
-            output += `<div id="box[${row}][${col}]" class='dark'></div>`;
-        }
-        output += "</div>";
-    }
-    document.getElementById('griddle').innerHTML = output;
-    
-    griddle.forEach((div) => { 
-        div.addEventListener('mouseover', (e) => {if (mouseDown == true) {
-            e.target.classList.add(`light`)}})
-        //div.addEventListener('mouseover', hueStroke)
-    })
-    
-}
-
-drawGrid(square);
+};
