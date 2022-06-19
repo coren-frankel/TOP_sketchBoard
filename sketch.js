cfd4dfe22a3fff9dc445a036f32a81462c8ed967
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-06-19 15:18:21 -0600

    Fix border generation; Next work on buttons

--- sketch.js
+++ sketch.js
@@ -1,43 +1,49 @@
-let grid = document.getElementById('grid');
-var slider = document.getElementById("myRange"); 
-var color = 'dark';
-var side = slider.value;
-let mouseDown = false; 
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+let grid = document.getElementById('grid'), slider = document.getElementById("myRange"); 
+//DOM selectors for grid creation and adjustment
+var canvas = 'dark', side = slider.value;
+//Establish color variable hosting default, and set dimensions tied to slider value
 drawGrid();
+
 slider.oninput = function() {
-    side = slider.value;
-    wipeGrid();
-    drawGrid();
+    wipeGrid();//Adjust slider and get a new grid!
 }    
-function wipeGrid() {
+function wipeGrid() {//Wipes previous grid and draws new one with the slider.value
     grid.innerHTML = '';
+    side = slider.value;
     drawGrid();
 }
-function drawGrid() {
+function drawGrid() {//Set dimensions of grid per it's container
     let dimens = document.getElementById('grid').clientWidth / side;
     grid.style.gridTemplateColumns = `repeat(${side - 3}, ${dimens}px) 1fr 1fr 1fr`;
     grid.style.gridTemplateRows = `repeat(${side - 3}, ${dimens}px) 1fr 1fr 1fr`;
     for(let i = 0; i < (side ** 2); i++){
         let square = document.createElement('div')
         grid.appendChild(square)
-        square.classList.add('dark')
-        square.classList.add('border-top-left');
+        square.classList.add(`${canvas}`)
+        square.classList.toggle('border-top-left');
     }
-    const edge = document.querySelectorAll(`.square:nth-child(${side}n)`);
+    const edge = grid.querySelectorAll(`.${canvas}:nth-child(${side}n)`);
     for (let i = 0; i < edge.length; i++) {
-    edge[i].setAttribute('data-right', 'true');
-    edge[i].classList.toggle('border-right');
+        //edge[i].setAttribute('data-right', 'true'); //what's the data attribute??
+        edge[i].classList.add('border-right');
     }
-    const stragglers = Array.from(grid).slice(-`${side}`);
+    const squares = grid.querySelectorAll('div');
+    const stragglers = Array.from(squares).slice(-`${side}`);
     for (let i = 0; i < stragglers.length; i++) {
-        stragglers[i].setAttribute('data-bottom', 'true');
-        stragglers[i].classList.toggle('border-bottom');
+        
+        //stragglers[i].setAttribute('data-bottom', 'true'); //what's the data attribute??
+        stragglers[i].classList.add('border-bottom');
     }
+    clickDraw();
+    
+}
+function clickDraw() {//McGraw
+    let mouseDown = false; 
+    document.body.onmousedown = () => (mouseDown = true);
+    document.body.onmouseup = () => (mouseDown = false);
     let squares = grid.querySelectorAll('div')
     squares.forEach((square) => { 
-        square.addEventListener('mouseover', (e) => {if (mouseDown == true) {
+        square.addEventListener('mousemove', (e) => {if (mouseDown == true) {
             e.target.classList.add(`light`)
         }})
     })
@@ -45,12 +51,22 @@ function drawGrid() {
 
 function canvasColor() {
     //if (e.type === 'mouseover' && !mouseDown) return
-    switch (color) {
-        case (color == 'dark'):
-            color = 'light';
-            return color;
-        case (color == 'light'):
-            color = 'dark';
-            return color;
+    let squares = grid.querySelectorAll('div');
+    switch (canvas) {
+        case (canvas == 'dark'):
+            canvas = 'light';
+            wipeGrid();
+            //squares.classList.add('light')
+        case (canvas == 'light'):
+            canvas = 'dark';
+            wipeGrid();
+            //squares.classList.add('dark')
+
     }
 }
+function sansBord() {
+    //let squares = grid.querySelectorAll('div');
+    squares.classList.remove('border-top-left');
+    squares.classList.remove('border-bottom');
+    squares.classList.remove('border-right');
+};
